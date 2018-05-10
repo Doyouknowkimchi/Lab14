e6bd504a16cc6f649f007a7e28726ec593a07872
Doyouknowkimchi <21700268@handong.edu>
2018-05-10 21:58:48 +0900

    second

--- src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/Bound_main.java
+++ src/main/java/edu/handong/csee/java/exceptionhandle/example/prob4/Bound_main.java
@@ -1,26 +1,26 @@
 package edu.handong.csee.java.exceptionhandle.example.prob4;
 import java.util.Scanner;
 
-public class Bound_main {
+public class Bound_main { // class name
 
-	public static void main(String[] args)
+	public static void main(String[] args) //main method
 	{
-		Scanner keyboard = new Scanner(System.in);
-		int num = 0;
-		Boundary arr = new Boundary();
-		boolean repeat = true;
-		while(repeat)
+		Scanner keyboard = new Scanner(System.in); // ready for input from keyboard
+		int num = 0; // variable
+		Boundary arr = new Boundary(); //construct
+		boolean repeat = true; // set true
+		while(repeat) // condition
 		{
-			try
+			try // try block
 			{
-				System.out.print("Enter an integer: ");
-				num = keyboard.nextInt();
-				arr.receive(num);
+				System.out.print("Enter an integer: "); // show text
+				num = keyboard.nextInt(); // get num
+				arr.receive(num); // call method
 			}
-			catch(ArrayIndexOutOfBoundsException e)
+			catch(ArrayIndexOutOfBoundsException e) //catch block
 			{
-				System.out.println("Invalid array index access!");
-				repeat = false;
+				System.out.println("Invalid array index access!"); //show text
+				repeat = false; // set false
 			}
 		}
 	}
